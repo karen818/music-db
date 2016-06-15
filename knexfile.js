b08eb868f2b2d857be74c7ddafb3206316158f5d
Christopher Anderson <Christopher@ImTheC.com>
2016-06-15 09:44:54 -0500

    Add initial setup

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/music'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+
+};
